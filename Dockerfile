62a4c345a5b62bd7d971d8092b541b1bb0c696d3
Liliia Chulei <liliia.chulei@gmail.com>
2024-11-12 01:41:09 +0200

    changes

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM node:18-alpine
+FROM node:14
 WORKDIR /app
 COPY package*.json ./
 RUN npm install
+# RUN npm run build
+# RUN npm build
 COPY . .
 EXPOSE 3000
 CMD ["npm", "start"]
---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.0" 
+    }
+  }
+}
+
+provider "docker" {
+  registry_auth {
+    address  = "https://index.docker.io/v1/"
+    username = var.dockerhub_username
+    password = var.dockerhub_password
+  }
+}
+
+resource "docker_image" "my_app" {
+  name = "lchulei/my_app:latest"
+  build {
+    context    = ""
+    dockerfile = "./Dockerfile"
+  }
+}
+
+variable "dockerhub_username" {
+  description = "Ім'я користувача DockerHub"
+}
+
+variable "dockerhub_password" {
+  description = "Пароль DockerHub"
+  sensitive   = true
+}
